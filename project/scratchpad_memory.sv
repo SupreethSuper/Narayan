648005ec0af42532311dad58b6fa10ca4be0d0be
SuperSupreeth <supreeth.sandri@gmail.com>
2026-06-05 08:07:41 -0700

    fsm logic change

--- project/scratchpad_memory.sv
+++ project/scratchpad_memory.sv
@@ -63,10 +63,10 @@ module scratchpad_memory #(
         else begin
             case (fsm_state)
                 RESET_STATE: begin
-                    if (!rw)
-                        next_fsm_state = WRITE_STATE;
+                    if (rw_)
+                        next_fsm_state = READ_STATE;
                     else
-                        next_fsm_state = RESET_STATE;
+                        next_fsm_state = WRITE_STATE;
                     // next_fsm_state = RESET_STATE || WRITE_STATE;
                 end
 
